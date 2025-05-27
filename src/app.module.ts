d20aa8de515a3583e87a0ec8bc2474c35c792eed
murodjon <smurodjon983@gmail.com>
2025-05-27 15:36:20 +0500

    Project started 26.05.2025

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.PG_HOST,
+      port: Number(process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASS,
+      database: process.env.PG_DB,
+      logging: false,
+      synchronize: true,
+      autoLoadModels: true,
+      models: []
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
